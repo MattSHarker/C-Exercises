14895bc8fb0169e24648746e15e56ceb5af559f2
MattSHarker <mattsharker@gmail.com>
2019-03-19 18:54:58 -0700

    Create 1-2.c
    Completed the second exercise of chapter 1

---
+++ Chapter 1/1-2.c
@@ -0,0 +1,13 @@
+
+// Matthew Harker
+// Chapter 1 Exercise 2
+
+#include <stdio.h>
+
+int main()
+{
+    printf("hello, wo\crld\n");
+}
+
+// \c is not a recognized escape command
+// compiler (gcc) throws a warning then prints the c in the string
